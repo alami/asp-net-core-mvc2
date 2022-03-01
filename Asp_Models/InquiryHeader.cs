b425bb61621d1935af27c6cde53f927a59729dee
aminov <alami@tshtt.uz>
2022-03-01 11:23:20 +0500

    2-ch04-03 new tbl InquiryHeader & Detail & migration

---
+++ Asp_Models/InquiryHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp_Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime InquiryDate { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
