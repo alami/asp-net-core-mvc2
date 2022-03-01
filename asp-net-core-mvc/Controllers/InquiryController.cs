969033e4686e62faf3495542486a8704917eaa33
aminov <alami@tshtt.uz>
2022-03-01 18:57:52 +0500

    2-ch04-008-fin start AdminDashbord: InquiryVM & Ctor

---
+++ asp-net-core-mvc/Controllers/InquiryController.cs
@@ -0,0 +1,21 @@
+﻿using Asp_DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_core_mvc.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+        public InquiryController(IInquiryDetailRepository inqDRepo,
+            IInquiryHeaderRepository inqHRepo)
+        {
+            _inqDRepo = inqDRepo;
+            _inqHRepo = inqHRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
