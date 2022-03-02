06535c9ae08c01cd1f27ebf480b385bd23b942b2
aminov <alami@tshtt.uz>
2022-03-02 11:17:48 +0500

    2-ch05-005 Inquiry-Details

--- asp-net-core-mvc/Controllers/InquiryController.cs
+++ asp-net-core-mvc/Controllers/InquiryController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp_DataAccess.Repository.IRepository;
+using Asp_Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace asp_net_core_mvc.Controllers
@@ -7,6 +8,8 @@ namespace asp_net_core_mvc.Controllers
     {
         private readonly IInquiryHeaderRepository _inqHRepo;
         private readonly IInquiryDetailRepository _inqDRepo;
+        [BindProperty]
+        public InquiryVM InquiryVM { get; set; }
         public InquiryController(IInquiryDetailRepository inqDRepo,
             IInquiryHeaderRepository inqHRepo)
         {
@@ -17,6 +20,17 @@ namespace asp_net_core_mvc.Controllers
         {
             return View();
         }
+        public IActionResult Details(int id)
+        {
+            InquiryVM = new InquiryVM()
+            {
+                InquiryHeader = _inqHRepo.FirstOrDefault(u => u.Id == id),
+                InquiryDetail = _inqDRepo.GetAll(u => u.InquiryHeaderId == id, includeProperties: "Product")
+            };
+            return View(InquiryVM);
+        }
+
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetInquiryList()
