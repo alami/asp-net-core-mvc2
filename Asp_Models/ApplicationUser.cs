3528029b5ef7cf4ef1915671ccecdfd9c2ca26b6
aminov <alami@tshtt.uz>
2022-03-03 10:46:08 +0500

    2-ch07-002 Model & Sets of OrderHeader And Detail To Db

--- Asp_Models/ApplicationUser.cs
+++ Asp_Models/ApplicationUser.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Asp_Models
 {
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
+        [NotMapped]
+        public string StreetAddress { get; set; }
+        [NotMapped]
+        public string City { get; set; }
+        [NotMapped]
+        public string State { get; set; }
+        [NotMapped]
+        public string PostalCode { get; set; }
     }
 }
